d1ba130f650199233d9c4fe745c5bc59a9924c71
EnhancedRobot <97986211+EnhancedRobot@users.noreply.github.com>
2025-12-25 22:54:17 -0500

    Started adding castling

--- src/main/java/net/ics4u/summativechess/game/pieces/base/King.java
+++ src/main/java/net/ics4u/summativechess/game/pieces/base/King.java
@@ -7,6 +7,7 @@ package main.java.net.ics4u.summativechess.game.pieces.base;
 import java.util.LinkedList;
 import java.util.List;
 import main.java.net.ics4u.summativechess.game.board.Board;
+import main.java.net.ics4u.summativechess.game.board.tiles.Tile;
 import main.java.net.ics4u.summativechess.util.BoardPos;
 
 /**
@@ -41,9 +42,66 @@ public class King extends Piece {
         }
         
         // Handle castling
-
+        if(timesMoved == 0) {
+            BoardPos check = new BoardPos(position);
+            
+            // For every direction
+            for (BoardPos direction : BoardPos.DIRECTIONS) {
+                // While still in the board
+                while(board.isInBoard(check)) {
+                    // Move forward in the direction
+                    check.add(direction);
+                    
+                    // Get the tile at the position
+                    Tile tile = board.getTile(check);
+                    
+                    // If the tile is non-traversable
+                    if(tile != null && !tile.isTraversable) {
+                        // Break
+                        break;
+                    }
+                    
+                    // Get the piece on the tile
+                    Piece onTile = board.getPiece(check);
+                    
+                    // If there is a piece on the tile
+                    if(onTile != null) {
+                        // If the piece on the tile is a rook that has never moved
+                        if(onTile.id.equals("R") && onTile.timesMoved == 0) {
+                            // Add the castling
+                            
+                            // Get two spaces in that direction
+                            BoardPos goTo = new BoardPos(direction);
+                            goTo = goTo.multiply(2);
+                            
+                            // Add the current position to get the point you would castle to
+                            goTo.add(position);
+                            
+                            // Add the position to the list of possible moves
+                            moves.add(goTo);
+                        }
+                        
+                        // We've hit something, so break
+                        break;
+                    }
+                }
+            }
+        }
              
         // Return the list of places this piece can go to
         return moves;
+    }
+    
+    
+    @Override
+    public void onMoveTo(BoardPos position) {
+        // Subtract the original position from the new position to get the relative position
+        BoardPos moved = new BoardPos(position).subtract(this.position);
+        
+        // If we moved two tiles (that's a castle... Probably.)
+        // This may be a problem I need to solve
+        if(moved.maxDistance() == 2) {
+            
+        }
     }
 }
